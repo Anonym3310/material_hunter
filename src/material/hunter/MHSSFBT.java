b036b4b5150237a7aac70a58c97e7ec301f821d2
Иван <54078891+Mirivan@users.noreply.github.com>
2021-12-11 21:53:25 +0500

    removed unused timer

--- src/material/hunter/MHSSFBT.java
+++ src/material/hunter/MHSSFBT.java
@@ -2,7 +2,6 @@ package material.hunter;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,10 +10,8 @@ public class MHSSFBT extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pelmeshek);
-        new Handler().postDelayed((Runnable) () -> {
-            Intent i = new Intent(this, AppNavHomeActivity.class);
-            startActivity(i);
-            finish();
-        }, (long) (1.5 * 1000));
+        Intent i = new Intent(this, AppNavHomeActivity.class);
+        startActivity(i);
+        finish();
     }
 }
